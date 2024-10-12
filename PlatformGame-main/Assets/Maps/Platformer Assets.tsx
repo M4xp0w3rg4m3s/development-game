8cc72bc22f2cda44d2ca5060e90d9f7531c82d97
M4xp0w3rg4m3s <156011390+M4xp0w3rg4m3s@users.noreply.github.com>
2024-10-12 13:03:32 +0200

    Fixed Tilemap & Tileset + rendering it

--- PlatformGame-main/Assets/Maps/Platformer Assets.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Platformer Assets" tilewidth="32" tileheight="32" tilecount="1080" columns="27">
- <image source="GandalfHardcore Platformer Assets.png" width="864" height="1280"/>
-</tileset>
