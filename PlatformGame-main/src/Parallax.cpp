faa21f83f8011c7c83d9e51abed58b766401ff88
Guillman07 <guillem.ma.dev@gmail.com>
2025-01-11 21:24:19 +0100

    Parallax Improved

--- PlatformGame-main/src/Parallax.cpp
+++ PlatformGame-main/src/Parallax.cpp
@@ -1,119 +1,46 @@
 #include "Parallax.h"
-
 #include "Entity.h"
 #include "Sprite.h"
 #include "Engine.h"
 #include "Textures.h"
 #include "Log.h"
 
-Parallax::Parallax()
-{
+Parallax::Parallax() : previousCamX(0.0f) {
+    layers.resize(5);
 }
 
-bool Parallax::ChangeTextures()
-{
-		layer1.texture = Engine::GetInstance().textures->Load(textureName1.c_str());
-		layer1.speed = 0;
-		layer1.offset = 0;
+bool Parallax::ChangeTextures() {
+    std::vector<std::string> textureNames = { textureName1, textureName2, textureName3, textureName4, textureName5 };
+    std::vector<float> speeds = { 0.0f, 0.1f, 0.3f, 0.5f, 0.7f };
 
-		layer2.texture = Engine::GetInstance().textures->Load(textureName2.c_str());
-		layer2.speed = 0.1f;
-		layer2.offset = 0;
-
-		layer3.texture = Engine::GetInstance().textures->Load(textureName3.c_str());
-		layer3.speed = 0.3f;
-		layer3.offset = 0;
-
-		layer4.texture = Engine::GetInstance().textures->Load(textureName4.c_str());
-		layer4.speed = 0.5f;
-		layer4.offset = 0;
-
-		layer5.texture = Engine::GetInstance().textures->Load(textureName5.c_str());
-		layer5.speed = 0.7f;
-		layer5.offset = 0;
-
-		return true;
-
-		return true;
-	}
-
-bool Parallax::Start()
-{
-	layer1.texture = Engine::GetInstance().textures->Load(textureName1.c_str());
-	layer1.speed = 0;
-	layer1.offset = 0;
-
-	layer2.texture = Engine::GetInstance().textures->Load(textureName2.c_str());
-	layer2.speed = 0.1f;
-	layer2.offset = 0;
-
-	layer3.texture = Engine::GetInstance().textures->Load(textureName3.c_str());
-	layer3.speed = 0.3f;
-	layer3.offset = 0;
-
-	layer4.texture = Engine::GetInstance().textures->Load(textureName4.c_str());
-	layer4.speed = 0.5f;
-	layer4.offset = 0;
-
-	layer5.texture = Engine::GetInstance().textures->Load(textureName5.c_str());
-	layer5.speed = 0.7f;
-	layer5.offset = 0;
-
-	return true;
+    for (size_t i = 0; i < layers.size(); ++i) {
+        layers[i].texture = Engine::GetInstance().textures->Load(textureNames[i].c_str());
+        layers[i].speed = speeds[i];
+        layers[i].offset = 0.0f;
+    }
+    return true;
 }
 
-bool Parallax::Update(float dt)
-{
+bool Parallax::Start() {
+    return ChangeTextures();
+}
 
-	layer1.offset += ((float)Engine::GetInstance().render->camera.x - previousCamX) * layer1.speed;
-	if (abs(layer1.offset) > sizeW) {
-		layer1.offset = 0;
-	}
-	layer2.offset += ((float)Engine::GetInstance().render->camera.x - previousCamX) * layer2.speed;
-	if (abs(layer2.offset) > sizeW) {
-		layer2.offset = 0;
-	}
-	layer3.offset += ((float)Engine::GetInstance().render->camera.x - previousCamX) * layer3.speed;
-	if (abs(layer3.offset) > sizeW) {
-		layer3.offset = 0;
-	}
-	layer4.offset += ((float)Engine::GetInstance().render->camera.x - previousCamX) * layer4.speed;
-	if (abs(layer4.offset) > sizeW) {
-		layer4.offset = 0;
-	}
-	layer5.offset += ((float)Engine::GetInstance().render->camera.x - previousCamX) * layer5.speed;
-	if (abs(layer5.offset) > sizeW) {
-		layer5.offset = 0;
-	}
+bool Parallax::Update(float dt) {
+    auto& engine = Engine::GetInstance();
+    float cameraX = static_cast<float>(engine.render->camera.x);
+    float deltaCamX = cameraX - previousCamX;
+    SDL_Rect rect = { 0, 0, sizeW, sizeH };
 
-	SDL_Rect rect = { 0, 0, sizeW, sizeH };
+    for (auto& layer : layers) {
+        layer.offset += deltaCamX * layer.speed;
+        if (fabs(layer.offset) > sizeW) {
+            layer.offset = 0.0f;
+        }
+        for (int i = -1; i <= 1; ++i) {
+            engine.render->DrawTexture(layer.texture, -(int)cameraX + (int)(layer.offset + i * sizeW), (int)engine.render->camera.y, &rect, SDL_FLIP_NONE);
+        }
+    }
 
-
-	Engine::GetInstance().render->DrawTexture(layer1.texture, -(int)Engine::GetInstance().render->camera.x + (int)layer1.offset, (int)Engine::GetInstance().render->camera.y, &rect, (SDL_RendererFlip)0);
-	Engine::GetInstance().render->DrawTexture(layer1.texture, -(int)Engine::GetInstance().render->camera.x + (int)(layer1.offset - sizeW), (int)Engine::GetInstance().render->camera.y, &rect, (SDL_RendererFlip)0);
-	Engine::GetInstance().render->DrawTexture(layer1.texture, -(int)Engine::GetInstance().render->camera.x + (int)(layer1.offset + sizeW), (int)Engine::GetInstance().render->camera.y, &rect, (SDL_RendererFlip)0);
-
-
-	Engine::GetInstance().render->DrawTexture(layer2.texture, -(int)Engine::GetInstance().render->camera.x + (int)layer2.offset, (int)Engine::GetInstance().render->camera.y, &rect, (SDL_RendererFlip)0);
-	Engine::GetInstance().render->DrawTexture(layer2.texture, -(int)Engine::GetInstance().render->camera.x + (int)(layer2.offset - sizeW), (int)Engine::GetInstance().render->camera.y, &rect, (SDL_RendererFlip)0);
-	Engine::GetInstance().render->DrawTexture(layer2.texture, -(int)Engine::GetInstance().render->camera.x + (int)(layer2.offset + sizeW), (int)Engine::GetInstance().render->camera.y, &rect, (SDL_RendererFlip)0);
-
-
-	Engine::GetInstance().render->DrawTexture(layer3.texture, -(int)Engine::GetInstance().render->camera.x + (int)layer3.offset, (int)Engine::GetInstance().render->camera.y, &rect, (SDL_RendererFlip)0);
-	Engine::GetInstance().render->DrawTexture(layer3.texture, -(int)Engine::GetInstance().render->camera.x + (int)(layer3.offset - sizeW), (int)Engine::GetInstance().render->camera.y, &rect, (SDL_RendererFlip)0);
-	Engine::GetInstance().render->DrawTexture(layer3.texture, -(int)Engine::GetInstance().render->camera.x + (int)(layer3.offset + sizeW), (int)Engine::GetInstance().render->camera.y, &rect, (SDL_RendererFlip)0);
-
-
-	Engine::GetInstance().render->DrawTexture(layer4.texture, -(int)Engine::GetInstance().render->camera.x + (int)layer4.offset, (int)Engine::GetInstance().render->camera.y, &rect, (SDL_RendererFlip)0);
-	Engine::GetInstance().render->DrawTexture(layer4.texture, -(int)Engine::GetInstance().render->camera.x + (int)(layer4.offset - sizeW), (int)Engine::GetInstance().render->camera.y, &rect, (SDL_RendererFlip)0);
-	Engine::GetInstance().render->DrawTexture(layer4.texture, -(int)Engine::GetInstance().render->camera.x + (int)(layer4.offset + sizeW), (int)Engine::GetInstance().render->camera.y, &rect, (SDL_RendererFlip)0);
-
-
-	Engine::GetInstance().render->DrawTexture(layer5.texture, -(int)Engine::GetInstance().render->camera.x + (int)layer5.offset, (int)Engine::GetInstance().render->camera.y, &rect, (SDL_RendererFlip)0);
-	Engine::GetInstance().render->DrawTexture(layer5.texture, -(int)Engine::GetInstance().render->camera.x + (int)(layer5.offset - sizeW), (int)Engine::GetInstance().render->camera.y, &rect, (SDL_RendererFlip)0);
-	Engine::GetInstance().render->DrawTexture(layer5.texture, -(int)Engine::GetInstance().render->camera.x + (int)(layer5.offset + sizeW), (int)Engine::GetInstance().render->camera.y, &rect, (SDL_RendererFlip)0);
-
-	previousCamX = (float)Engine::GetInstance().render->camera.x;
-
-	return true;
+    previousCamX = cameraX;
+    return true;
 }
--- PlatformGame-main/src/Parallax.h
+++ PlatformGame-main/src/Parallax.h
@@ -5,40 +5,29 @@
 #include "SDL2/SDL_render.h"
 #include <box2d/box2d.h>
 #include <string>
+#include <vector>
+
+struct Layer {
+	SDL_Texture* texture = nullptr;
+	float speed = 0.0f;
+	float offset = 0.0f;
+};
 
 class Parallax : public Module {
 public:
-
 	Parallax();
-	~Parallax() {}
-
-	struct Layer {
-
-		float offset;
-		float speed;
-		SDL_Texture* texture;
-	};
-
-	bool ChangeTextures();
-	
 	bool Start();
 	bool Update(float dt);
+	bool ChangeTextures();
 
-public:
+	std::vector<Layer> layers;
+	float previousCamX;
+	int sizeW = 1421;  // Example default width
+	int sizeH = 480;  // Example default height
 
-	Layer layer1{0,0,nullptr};
 	std::string textureName1;
-	Layer layer2{ 0,0,nullptr };
 	std::string textureName2;
-	Layer layer3{ 0,0,nullptr };
 	std::string textureName3;
-	Layer layer4{ 0,0,nullptr };
 	std::string textureName4;
-	Layer layer5{ 0,0,nullptr };
 	std::string textureName5;
-
-	int sizeW = 1421;
-	int sizeH = 480;
-
-	float previousCamX = 0;
 };
